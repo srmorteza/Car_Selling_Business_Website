7fe66344cc7a840e12a99f517b047f54e71405b2
sr.morteza <sr.morteza@gmail.com>
2022-09-07 14:37:47 +0430

    add team modle and this database table to project 1401-6-16

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    fisrt_name=models.CharField(max_length= 255)
+    last_name=models.CharField(max_length= 255)
+    designation=models.CharField(max_length= 255)
+    photo = models.ImageField(upload_to='photos/%Y/%M/%D/')
+    facebook_link=models.URLField(max_length=100)
+    twitter_link=models.URLField(max_length=100)
+    googlePlus_link=models.URLField(max_length=100)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.fisrt_name
+
+
