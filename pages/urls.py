9387279f466875ad242e18be4809d4be62bbc179
sr.morteza <sr.morteza@gmail.com>
2022-09-07 10:09:09 +0430

    add about,services,contact template to project 1401-6-16

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name='home'),
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def Home(request):
+def home(request):
     return render(request, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
