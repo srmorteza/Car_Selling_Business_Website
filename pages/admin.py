783e9e11d4a0a441af2495f8955117ef0ca6dd8b
sr.morteza <sr.morteza@gmail.com>
2022-09-10 09:42:03 +0430

    add some chang to admin panel in team page to project 1401-6-19

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Team
+
+
 # Register your models here.
 
-admin.site.register(Team)
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius:50px;" />'.format(object.photo.url))
+
+    thumbnail.short_description = 'photo'
+
+    list_display = ('id', 'fisrt_name', 'last_name', 'designation', 'thumbnail', 'created_at')
+    list_display_links = ('id', 'fisrt_name',)
+    search_fields = ('fisrt_name', 'last_name', 'designation',)
+    list_filter = ('designation',)
+
+
+admin.site.register(Team, TeamAdmin)
