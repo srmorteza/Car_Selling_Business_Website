64acb1ada6c076985966920d44f019857a756f4a
sr.morteza <sr.morteza@gmail.com>
2022-09-26 12:19:46 +0330

    complated cars page with paginatior and compated car detail page 1401-7-4

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from cars.models import Car
+
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.order_by('created_at')
+    paginator = Paginator(cars, 1)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+
+    data = {
+        'cars': paged_cars,
+    }
+    return render(request, 'cars/cars.html', data)
+
+
+def car_detail(request, id):
+    single_car = get_object_or_404(Car, pk=id)
+    data = {
+        'single_car': single_car
+    }
+    return render(request, 'cars/car_detail.html', data)
