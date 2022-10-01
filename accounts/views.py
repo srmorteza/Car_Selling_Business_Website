48461921a4757c8d1913d7864c9c4a567bf5cf3b
sr.morteza <sr.morteza@gmail.com>
2022-10-01 10:37:37 +0330

    add login register dashroard template page to project 1401-7-9

---
+++ accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def register(request):
+    return render(request, 'accounts/register.html')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
--- car_selling/settings.py
+++ car_selling/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'multiselectfield',
     'django.contrib.humanize',
-
+    'accounts',
 
 ]
 
